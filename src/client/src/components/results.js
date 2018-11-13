c927a0afd78c77479d834d14fccb0b8aedd9e8c0
Patrick Ramser <pramser@onshift.com>
2018-11-13 15:20:19 -0500

    Hooking client up to server to test each piece end-to-end

--- src/client/src/components/results.js
+++ src/client/src/components/results.js
@@ -1,11 +1,30 @@
 // Dependencies
 import React, { Component } from "react";
+import { Button } from "reactstrap";
 
 class Results extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ results: [{ testName: "test" }] });
+  }
+
+  createResult = () => {
+    fetch("http://localhost:3001/results").then(() => {
+      alert("fetched!");
+    });
+  };
+
   render() {
     return (
       <div className="Results">
         <span>Hello, results!</span>
+        <Button onClick={this.createResult}>POST result</Button>
       </div>
     );
   }
