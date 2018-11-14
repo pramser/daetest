88651824fe602a315339720f68d3c369fb83853b
Patrick Ramser <pramser@onshift.com>
2018-11-14 16:29:04 -0500

    Test data for retrieving and displaying results

--- src/client/src/components/results.js
+++ src/client/src/components/results.js
@@ -11,7 +11,52 @@ class Results extends Component {
   }
 
   componentDidMount() {
-    this.setState({ results: [{ testName: "test" }] });
+    this.setState({
+      results: [
+        {
+          date: "2018-11-1",
+          product: "schedule",
+          type: "int",
+          passed: 100,
+          failed: 0
+        },
+        {
+          date: "2018-11-1",
+          product: "schedule",
+          type: "int",
+          passed: 100,
+          failed: 0
+        },
+        {
+          date: "2018-11-2",
+          product: "schedule",
+          type: "int",
+          passed: 95,
+          failed: 5
+        },
+        {
+          date: "2018-11-2",
+          product: "schedule",
+          type: "int",
+          passed: 100,
+          failed: 0
+        },
+        {
+          date: "2018-11-3",
+          product: "schedule",
+          type: "int",
+          passed: 100,
+          failed: 0
+        },
+        {
+          date: "2018-11-3",
+          product: "schedule",
+          type: "int",
+          passed: 100,
+          failed: 0
+        }
+      ]
+    });
   }
 
   createResult = () => {
@@ -23,7 +68,18 @@ class Results extends Component {
   render() {
     return (
       <div className="Results">
-        <span>Hello, results!</span>
+        <h1>Results</h1>
+        {this.state.results.map(
+          ({ date, product, type, passed, failed }, r) => (
+            <div className="result">
+              <span className="result-col">{date}</span>
+              <span className="result-col">{product}</span>
+              <span className="result-col">{type}</span>
+              <span className="result-col">{passed}</span>
+              <span className="result-col">{failed}</span>
+            </div>
+          )
+        )}
         <Button onClick={this.createResult}>POST result</Button>
       </div>
     );
