0c24f92b401813c38d1e47608cf7844a59303ad9
Patrick Ramser <pramser@onshift.com>
2018-12-03 12:39:51 -0500

    Removed omits from graph queries

--- src/server/src/resolvers/Query.ts
+++ src/server/src/resolvers/Query.ts
@@ -1,24 +1,17 @@
 /// <reference path="../schema.d.ts" />
-import { Omit } from 'graphql-yoga/dist/types';
 
 import { Result } from '../models/result';
 
 export const Query = {
   async allResults() {
-    const results = (await Result.query()) as Omit<
-      TestmonApi.IResult,
-      '__typename' | 'customers'
-    >[];
-    return results as Omit<TestmonApi.IQuery['allResults'], '__typename'>;
+    const results = await Result.query();
+    return results;
   },
   async resultById(_: any, args: TestmonApi.IResultByIdOnQueryArguments) {
     const result = await Result.query().findById(args.id as string);
     if (!result) {
       return null;
     }
-    return (result as Omit<
-      TestmonApi.IResult,
-      '__typename' | 'customers'
-    >) as Omit<TestmonApi.IQuery['resultById'], '__typename'>;
+    return result;
   }
 };
