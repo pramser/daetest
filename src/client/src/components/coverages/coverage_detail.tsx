2353834b5cee81161272d79e8ca8c65eb81c7626
Patrick Ramser <pramser@gmail.com>
2019-01-25 18:56:13 -0500

    Broke tabs out and formatted raw-data

--- src/client/src/components/coverages/coverage_detail.tsx
+++ src/client/src/components/coverages/coverage_detail.tsx
@@ -1,18 +1,18 @@
 // Dependencies
-import React, { Component } from "react";
-import { Nav, NavItem, NavLink, TabContent, TabPane, Table } from "reactstrap";
+import React, { Component } from 'react';
+import { Nav, NavItem, NavLink, TabContent, TabPane, Table } from 'reactstrap';
 
 // Types
-import { TestCase } from "../../types/Types";
+import { TestCase } from '../../types/Types';
 
 // Data
-import { coverage_data } from "../../repositories/coverage_repository";
+import { coverage_data } from '../../repositories/coverage_repository';
 
 class CoverageDetail extends Component<
   {},
   { coverage: TestCase[]; activeTab: string }
 > {
-  state = { coverage: [] as TestCase[], activeTab: "covs" as string };
+  state = { coverage: [] as TestCase[], activeTab: 'covs' as string };
 
   componentDidMount() {
     this.setState({
@@ -32,45 +32,59 @@ class CoverageDetail extends Component<
         <Nav tabs>
           <NavItem>
             <NavLink
-              active={this.state.activeTab === "covs"}
-              onClick={() => this.toggle("covs")}
+              active={this.state.activeTab === 'covs'}
+              onClick={() => this.toggle('covs')}
             >
               Report
             </NavLink>
           </NavItem>
           <NavItem>
             <NavLink
-              active={this.state.activeTab === "raw"}
-              onClick={() => this.toggle("raw")}
+              active={this.state.activeTab === 'raw'}
+              onClick={() => this.toggle('raw')}
             >
               Raw
             </NavLink>
           </NavItem>
         </Nav>
         <TabContent activeTab={this.state.activeTab}>
-          <TabPane tabId="covs">
-            <Table>
-              <thead>
-                <tr>
-                  <th>Test Name</th>
-                  <th>Result</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.coverage.map(({ test_name, result }) => (
-                  <tr>
-                    <td>{test_name}</td>
-                    <td>{result ? "Pass" : "Fail"}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
-          </TabPane>
-          <TabPane tabId="raw">{JSON.stringify(this.state.coverage)}</TabPane>
+          <CoverageTab coverage={this.state.coverage} />
+          <RawTab coverage={this.state.coverage} />
         </TabContent>
       </div>
     );
   }
 }
+
+const CoverageTab = (props: { coverage: TestCase[] }) => {
+  return (
+    <TabPane tabId="covs">
+      <Table>
+        <thead>
+          <tr>
+            <th>Test Name</th>
+            <th>Result</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.coverage.map(({ test_name, result }) => (
+            <tr>
+              <td>{test_name}</td>
+              <td>{result ? 'Pass' : 'Fail'}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </TabPane>
+  );
+};
+
+const RawTab = (props: { coverage: TestCase[] }) => {
+  return (
+    <TabPane tabId="raw">
+      <div className="raw-data">{JSON.stringify(props.coverage)}</div>
+    </TabPane>
+  );
+};
 
 export default CoverageDetail;
