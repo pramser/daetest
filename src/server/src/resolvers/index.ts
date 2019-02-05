eec75daf93735425887cb228e6464638c51acbfb
Patrick Ramser <pramser@onshift.com>
2019-02-05 13:41:24 -0500

    Renamed some files in order to rebuild schema

--- src/server/src/resolvers/index.ts
+++ src/server/src/resolvers/index.ts
@@ -4,6 +4,7 @@ import { TypeMap } from 'graphql/type/schema';
 
 import { Query } from './Query';
 import { Mutation } from './Mutation';
+// import { File } from './File';
 // import { Result } from './Result';
 
 export const resolvers: IResolvers<TypeMap> = {
@@ -12,5 +13,6 @@ export const resolvers: IResolvers<TypeMap> = {
   Time: GraphQLTime,
   Query: Query as any,
   Mutation: Mutation as any
+  //   File: File as any
   //   Result: Result as any
 };
