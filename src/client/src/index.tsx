2c77beab65ab7629c901300f2735170b620bd251
Patrick Ramser <pramser@onshift.com>
2019-02-19 16:52:27 -0500

    Removed yoga in favor of raw, Apollo implementation for uploads

--- src/client/src/index.tsx
+++ src/client/src/index.tsx
@@ -3,8 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+import { createUploadLink } from 'apollo-upload-client';
 
 // CSS
 import './index.css';
@@ -41,7 +45,21 @@ library.add(
   faVial
 );
 
-const client = new ApolloClient({ uri: 'http://localhost:4000' });
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    createUploadLink({ uri: 'http://localhost:4000' })
+  ]),
+  cache: new InMemoryCache()
+});
 
 // Entry-point for test-mon-client
 ReactDOM.render(
