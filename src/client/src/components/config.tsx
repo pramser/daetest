9873e76dd4a5f973b63be8fff2880fabfca0a995
Patrick Ramser <pramser@gmail.com>
2019-03-10 13:24:18 -0400

    Simple foundation for the config page

--- src/client/src/components/config.tsx
+++ src/client/src/components/config.tsx
@@ -1,9 +1,38 @@
 // Dependencies
 import React, { Component } from 'react';
 
-class Config extends Component {
+class Config extends Component<
+  any,
+  { alm: { name: string; url: string }; users: Array<any> }
+> {
+  state = {
+    alm: { name: '', url: '' },
+    users: []
+  };
+
+  componentDidMount() {
+    const alm = {
+      name: 'Jira',
+      url: 'https://company.atlassian.com/'
+    };
+
+    const users = [{ name: 'Patrick', email: 'pramser@gmail.com' }];
+    this.setState({ alm, users });
+  }
+
   render() {
-    return <div className="Config">Configuration</div>;
+    return (
+      <div className="Config">
+        <div className="alm">
+          <span>{this.state.alm.name}</span>
+        </div>
+        <div className="users">
+          {this.state.users.map((user: any) => (
+            <span>{user.name}</span>
+          ))}
+        </div>
+      </div>
+    );
   }
 }
 
