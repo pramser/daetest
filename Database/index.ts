2b63e9fae1499ccb8b28fbf5008dca8e87cff6f3
Patrick Ramser <pramser@Patricks-MacBook-Pro-2.local>
2020-05-17 11:51:47 -0400

    Reset to deno server-type

--- Database/index.ts
+++ Database/index.ts
@@ -1,34 +1,12 @@
-import { GraphQLServer } from 'graphql-yoga';
-import { Context, Options } from 'graphql-yoga/dist/types';
-import path from 'path';
-import pg from 'pg';
-import { parse as parseDatabaseUrl } from 'pg-connection-string';
+import { init as initDatabase } from "./database";
+import { init as initSeed } from "./seed";
 
-import { init } from './init';
-import { resolvers } from './resolvers';
-
-const parsed = parseDatabaseUrl(process.env.DATABASE_URL!);
-const pool = new pg.Pool({
-  host: parsed.host!,
-  port: parsed.port!,
-  user: parsed.user!,
-  password: parsed.password!,
-  database: parsed.database!
-});
-
-const options = {
-  cors: 'http://localhost:3000'
-} as Options;
-
-init().then(async () => {
-  const server = new GraphQLServer({
-    options: options,
-    typeDefs: path.resolve(__dirname, './schema.graphql'),
-    resolvers,
-    context: (req: Context) => ({ ...req, pool })
-  } as any);
-
-  server.start(options, ({ port }) =>
-    console.log(`Server is running on http://localhost:${port}`)
-  );
-});
+export async function init() {
+  let steps = [
+    initDatabase,
+    process.env.NODE_ENV === "development" && initSeed,
+  ].filter(Boolean);
+  for (const step of steps) {
+    step && (await step());
+  }
+}
