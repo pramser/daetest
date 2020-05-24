8e6da745e328893d0120031093cd7ce53c8c6151
Patrick Ramser <pramser@Patricks-MacBook-Pro-2.local>
2020-05-24 19:56:25 -0400

    Fix test icon name

--- ClientApp/src/components/test_icon.tsx
+++ ClientApp/src/components/test_icon.tsx
@@ -5,10 +5,11 @@ import { TestRunType } from "../types/Types";
 class TestIcon extends Component<{ type: TestRunType; size: string }, any> {
   render() {
     const { type, size } = this.props;
+    const name = TestRunType[type];
 
     return (
       <img
-        src={`${process.env.PUBLIC_URL}/images/${type}.png`}
+        src={`${process.env.PUBLIC_URL}/images/${name}.png`}
         height={size === "sm" ? "30" : "60"}
         alt="Test Icon"
       />
