6efeab24567a42e97ac64b773f36f596fe70816b
Patrick Ramser <pramser@Patricks-MacBook-Pro-2.local>
2020-05-24 20:06:06 -0400

    Refactor test_icon into FC

--- ClientApp/src/components/test_icon.tsx
+++ ClientApp/src/components/test_icon.tsx
@@ -1,20 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 
 import { TestRunType } from "../types/Types";
 
-class TestIcon extends Component<{ type: TestRunType; size: string }, any> {
-  render() {
-    const { type, size } = this.props;
-    const name = TestRunType[type];
-
-    return (
-      <img
-        src={`${process.env.PUBLIC_URL}/images/${name}.png`}
-        height={size === "sm" ? "30" : "60"}
-        alt="Test Icon"
-      />
-    );
-  }
+interface TestIconProps {
+  type: TestRunType;
+  size: string;
 }
+
+const TestIcon = ({ type, size }: TestIconProps) => {
+  const name = TestRunType[type];
+
+  return (
+    <img
+      src={`${process.env.PUBLIC_URL}/images/${name}.png`}
+      height={size === "sm" ? "30" : "60"}
+      alt="Test Icon"
+    />
+  );
+};
 
 export default TestIcon;
