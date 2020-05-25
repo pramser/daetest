237a9ddc6ccd50c320d1c4cfc7344946a6e23147
Patrick Ramser <pramser@Patricks-MacBook-Pro-2.local>
2020-05-24 20:07:49 -0400

    Refactor meta_icon into FC

--- ClientApp/src/components/meta_icon.tsx
+++ ClientApp/src/components/meta_icon.tsx
@@ -1,27 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 
-class MetaIcon extends Component<
-  { text: string; size: string; color: string },
-  any
-> {
-  render() {
-    const { text, size, color } = this.props;
-    const width = size === "sm" ? 30 : 60;
-    const height = width;
-
-    const style = {
-      color: color === "primary" || color === "secondary" ? "white" : "black",
-      backgroundColor: color === "primary" ? "#007bff" : "#6c757d",
-      width: width,
-      height: height,
-    };
-
-    return (
-      <div className="MetaIcon" style={style}>
-        {text}
-      </div>
-    );
-  }
+interface MetaIconProps {
+  text: string;
+  size: string;
+  color: string;
 }
+
+const MetaIcon = ({ text, size, color }: MetaIconProps) => {
+  const width = size === "sm" ? 30 : 60;
+  const height = width;
+
+  const style = {
+    color: color === "primary" || color === "secondary" ? "white" : "black",
+    backgroundColor: color === "primary" ? "#007bff" : "#6c757d",
+    width: width,
+    height: height,
+  };
+
+  return (
+    <div className="MetaIcon" style={style}>
+      {text}
+    </div>
+  );
+};
 
 export default MetaIcon;
