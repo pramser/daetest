26166f5031ddbdc1b2ac42649342e3af28f7f368
Patrick Ramser <pramser@Patricks-MacBook-Pro-2.local>
2020-05-24 20:04:18 -0400

    Make tests routing into FC

--- ClientApp/src/components/tests.tsx
+++ ClientApp/src/components/tests.tsx
@@ -1,18 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import { Route } from "react-router-dom";
 
 import RunList from "./run_list";
 import RunDetail from "./run_detail";
 
-class Tests extends Component<any, any> {
-  render() {
-    return (
-      <div>
-        <Route exact path={this.props.match.path} component={RunList} />
-        <Route path={`${this.props.match.url}/:runId`} component={RunDetail} />
-      </div>
-    );
-  }
-}
+const Tests = (props: any) => (
+  <div>
+    <Route exact path={props.match.path} component={RunList} />
+    <Route path={`${props.match.url}/:runId`} component={RunDetail} />
+  </div>
+);
 
 export default Tests;
