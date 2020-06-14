6550807462073c6b616e7f2eff5e4713acd096ff
Patrick Ramser <pramser@gmail.com>
2020-06-14 13:31:02 -0400

    Convert xml data to models in parser

--- parsers/junit_parser.ts
+++ parsers/junit_parser.ts
@@ -1,14 +1,29 @@
-import { IFileParser } from "../interfaces.ts";
+import { IFileParser, Test } from "../interfaces.ts";
 import parse from "https://denopkg.com/nekobato/deno-xml-parser/index.ts";
 
 export default class JUnitParser implements IFileParser {
-  public parseFile(file: string): [] {
+  public parseFile(file: string): Test[] {
     let xml = parse(file);
 
     if (xml.root && xml.root.name == "testsuites") {
-      return xml.root?.children as [];
+      return this.buildTests(xml.root?.children);
     }
 
     return [];
+  }
+
+  buildTests(objs: []): Test[] {
+    let tests: any = [];
+    objs.forEach((obj: any) => {
+      const test = new Test({
+        name: obj.attributes.name,
+        info: "",
+        description: "",
+        result: 0
+      })
+      tests.push(test)
+    });
+
+    return tests;
   }
 }
